c9769914780a2daf83f96499de0bcfafb2d4e7db
cyanomiko <cyanomiko@protonmail.com>
2021-10-04 00:37:37 +0300

    Move file receive routines to dedicated class

--- app/src/main/java/net/dcnnt/plugins/BaseFilePlugin.kt
+++ app/src/main/java/net/dcnnt/plugins/BaseFilePlugin.kt
@@ -1,10 +1,14 @@
 package net.dcnnt.plugins
 
 import android.content.ContentResolver
+import android.os.Environment
 import android.util.Log
+import androidx.documentfile.provider.DocumentFile
 import net.dcnnt.core.*
 import org.json.JSONObject
 import java.io.ByteArrayInputStream
+import java.io.File
+import java.nio.ByteBuffer
 
 
 abstract class BaseFilePlugin<T: PluginConf>(app: App, device: Device):
@@ -50,6 +54,69 @@ abstract class BaseFilePlugin<T: PluginConf>(app: App, device: Device):
         if (n.getInt("code") == 0) return DCResult(true, "OK")
         if (n.getInt("code") == 1) return DCResult(false, "Canceled")
         return DCResult(false,"Not confirmed")
+    }
+
+    protected fun safeFileName(downloadDir: DocumentFile, initialName: String): String? {
+        val existingNames: MutableSet<String> = mutableSetOf()
+        downloadDir.listFiles().forEach { f -> f.name?.also { existingNames.add(it) } }
+        if (!existingNames.contains(initialName)) return initialName
+        val file = File(initialName)
+        val name = file.nameWithoutExtension
+        val extension = file.extension
+        for (i in 1 .. 0xFFFF) {
+            val newName = "$name-$i.$extension"
+            if (!existingNames.contains(newName)) return newName
+        }
+        return null
+    }
+
+    fun recvFile(downloadDir: DocumentFile, entry: FileEntry, contentResolver: ContentResolver,
+                 progressCallback: (cur: Long, total: Long, part: Long) -> Unit,
+                 rpcMethod: String = "download"): DCResult {
+        if (Environment.getExternalStorageState() != Environment.MEDIA_MOUNTED) {
+            return DCResult(false,"Storage not mounted")
+        }
+        val fileName = safeFileName(downloadDir, entry.name)
+            ?: return DCResult(false, "No safe name for file")
+        val file = downloadDir.createFile("*/*", fileName)
+            ?: return DCResult(false, "Failed to create file")
+        val uri = file.uri
+        entry.localUri = uri
+        Log.d(TAG, "Open new file URI: $uri")
+        val fd = contentResolver.openOutputStream(uri)
+            ?: return DCResult(false, "Could not open file")
+        Log.d(TAG, "send request")
+        // ToDo: optionally replace index with something else
+        val resp = (rpc(rpcMethod, mapOf("index" to entry.remoteIndex, "size" to entry.size)) as? JSONObject)
+            ?: return DCResult(false,"Incorrect response")
+        var recBytes: Long = 0
+        var buf: ByteArray
+        Log.d(TAG, "resp = $resp")
+        if (resp.getInt("code") == 0) {
+            Log.d(TAG, "Start downloading: recBytes = $recBytes, entry.size = ${entry.size}")
+            var dataBuf: ByteBuffer? = null
+            if (entry.size < FileTransferPlugin.THUMBNAIL_THRESHOLD) {
+                entry.data = ByteArray(entry.size.toInt())
+                entry.data?.also { dataBuf = ByteBuffer.wrap(it) }
+            }
+            while (recBytes < entry.size) {
+                if (breakTransfer) {
+                    Log.i(TAG, "Downloading canceled")
+                    sock.close()
+                    return DCResult(false, "Canceled")
+                }
+//                Log.d(TAG, "recBytes = $recBytes, entry.size = ${entry.size}")
+                buf = read()
+                fd.write(buf)
+                dataBuf?.put(buf)
+                recBytes += buf.size
+                progressCallback(recBytes, entry.size, PART.toLong())
+            }
+            rpcSend("confirm", mapOf("code" to 0))
+            entry.status = FileStatus.DONE
+            return DCResult(true,"OK", data = entry.name)
+        }
+        return DCResult(false,"Request rejected")
     }
 
 }
